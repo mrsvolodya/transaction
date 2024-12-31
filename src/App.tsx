ff196984e19953ea067bdc76b8dda72a59258f7d
mrsvolodya <vovamursky@gmail.com>
2024-12-31 12:57:20 +0200

    second comit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import TransactionsPage from './pages/TransactionsPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import TransactionsPage from "./pages/TransactionsPage";
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/transactions" element={<TransactionsPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        {/* <Route path="/" element={<HomePage />} />
+        <Route path="/transactions" element={<TransactionsPage />} /> */}
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
